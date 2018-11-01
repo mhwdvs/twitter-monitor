7888731daa8b3cd3205612399a3666cf30056b44
ized <30953120+izedout@users.noreply.github.com>
2018-11-01 22:18:23 +0800

    Update start.py
    everything is working until the subprocess, have to test in fresh env because i think it is an issue with enviroment variables

--- start.py
+++ start.py
@@ -1,23 +1,24 @@
 import subprocess
 
 yn = 0
-usrsubmit = 0
-while yn = 0
+while yn == 0:
   handles = input("What is/are the desired twitter handle/s? (DO NOT include @, seperate multiple handles with a space)")
   type(handles)
   handles = handles.split(" ")
-  print("Handles Submitted: " + handles)
+  print("Handles Submitted: " + str(handles))
   print("Is this correct? Press *y* to continue or *n* to try again!")
-  type(usrsubmit)
+  usrsubmit = input()
   #checks are untested
-  if usrsubmit = "y"
+  if usrsubmit == "y":
     yn = 1
-  else yn = 0
+  else:
+    yn = 0
+    print("Restarting...")
 
-  #this subprocess is untested
+#this subprocess is untested
 i = 0
-  while i < len(handles):
-  subprocess.call(["python main.py", "-" + "handles[i]"])
+while i < len(handles):
+  subprocess.call(["python main.py", "-" + handles[i]], shell=True)
   i = i + 1
 
 print("All processes started!")
